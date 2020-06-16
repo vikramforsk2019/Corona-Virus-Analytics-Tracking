e3b8a57898b223039c58bfc3fd334fbbac64bf63
vikramforsk2019 <vsg16492cse2016@gmail.com>
2020-06-16 15:32:31 +0530

    world corona

---
+++ world_covid19.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 19:07:34 2020
+
+@author: jagveer
+"""
+
+import pandas as pd
+from selenium import webdriver
+from collections import OrderedDict
+#from bs4 import BeautifulSoup as BS
+url="https://www.worldometers.info/coronavirus/"
+#driver path
+browser = webdriver.Firefox(executable_path="/home/jagveer/anaconda3/geckodriver")
+browser.get(url)
+right_table=browser.find_element_by_class_name('table')
+
+column_name=right_table.find_elements_by_tag_name('th') #Give column name
+col_name=[]
+for i in range(1,len(column_name)-1):
+    col_name.append(column_name[i].text)
+    
+get_corona = browser.find_element_by_xpath('/html/body/div[3]/div[3]/div/div[4]/div[1]/div/table/tbody[1]')
+right_table=get_corona.find_elements_by_tag_name('tr')
+
+data_list=[]
+data_list2=[]
+for row in right_table:
+  cells = row.find_elements_by_tag_name('td')
+  for i in  cells:    
+     data_list.append(i.text)
+  data_list2.append(data_list)
+  data_list=[]
+df2=pd.DataFrame(data_list2)  
+    
